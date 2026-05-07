3f4c6abb218be9a42d6b0797244c81fc07fc39fd
Raman-Khiva <ramandeep01167@gmail.com>
2026-05-07 10:27:55 +0000

    itegrate api to table

--- apps/web/app/field/page.jsx
+++ apps/web/app/field/page.jsx
@@ -2,21 +2,42 @@
 import { LoginForm } from "@workspace/ui/components/login-form"
 
 import { Terminal } from "@workspace/ui/components/terminal"
+import { TableDemo } from "@workspace/ui/components/table-demo"
 import { TerminalDemo } from "@workspace/ui/components/terminal-demo"
 import { PortfolioTerminal } from "@workspace/ui/components/portfolio-terminal"
 import { MainTerminal } from "@workspace/ui/components/main-terminal"
 
 import axios from "axios"
 
+import { useState, useEffect } from "react"
 const page = () => {
+  const [users, setUsers] = useState([{ id: 1, name: "raman" }])
+
+  const fetchUsersFromDB = async () => {
+    try {
+      let res = await axios.post("http://34.131.111.242:5000/api/execute", {
+        query: "SELECT * FROM users",
+      })
+      res = res.data
+
+      console.log("Data fetched successfully")
+      console.log("Users", res.data.result)
+      setUsers(res.data.result)
+    } catch (err) {
+      console.error("Error while fetch users data from DB")
+    }
+  }
   const handleSubmit = async (query) => {
     try {
-      const res = await axios.post("http://34.131.111.242:5000/api/execute", {
+      let res = await axios.post("http://34.131.111.242:5000/api/execute", {
         query,
       })
+
+      res = res.data
       if (res.success) {
         console.log(res.message)
         console.log(res.data.result)
+        fetchUsersFromDB()
       } else {
         console.error("Unexpected error while exectuing query")
       }
@@ -25,10 +46,17 @@ const page = () => {
     }
   }
 
+  useEffect(() => {
+    fetchUsersFromDB()
+  }, [])
+
   return (
-    <div>
+    <div className="w-screen">
       <p>This is feild page</p>
-      <MainTerminal onSubmit={handleSubmit} />
+      <div className="flex justify-around">
+        <MainTerminal className="flex-1" onSubmit={handleSubmit} />
+        <TableDemo className="flex-1" users={users} />
+      </div>
     </div>
   )
 }
